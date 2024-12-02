a38ff1fa4ed8703b7acc37e4eb9c1e87b9fe9df8
Dey-Jess <ortegadeivids4@gmail.com>
2024-12-02 10:31:14 -0500

    Feat: Refactor portfolio page: se trasladaron los datos del proyecto a un módulo separado y se actualizaron los enlaces de la tarjeta del proyecto para mejorar la navegación.

---
+++ src/data/projects.ts
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    id: 1,
+    image: {
+      src: '/MoweCompany/matBento.png',
+      width: 1080,
+      height: 720,
+      format: 'webp',
+      alt: 'Proyecto 1'
+    },
+    title: 'MAT COMPANY',
+    description: 'Comercio, accesibilidad y tecnología en un solo lugar.',
+    url: 'https://mowecompany.github.io/MAT/'
+  },
+//   {
+//     id: 2,
+//     image: {
+//       src: 'https://bentogrids.com/cdn-cgi/image/width=1080,metadata=none,fit=scale-down,format=auto/https://s.bentogrids.com/cm3yrqqpd0000bvjb9lamibue.png', 
+//       width: 1080,
+//       height: 720,
+//       format: 'webp',
+//       alt: 'Proyecto 2'
+//     },
+//     title: 'Proyecto 2',
+//     description: 'Descripción del proyecto 2.',
+//   },
+//   {
+//     id: 3,
+//     image: {
+//       src: 'https://bentogrids.com/cdn-cgi/image/width=1080,metadata=none,fit=scale-down,format=auto/https://s.bentogrids.com/clzju3zsd000k12leqjal960w.png',
+//       width: 1080, 
+//       height: 720,
+//       format: 'webp',
+//       alt: 'Proyecto 3'
+//     },
+//     title: 'Proyecto 3',
+//     description: 'Descripción del proyecto 3.',
+//   },
+//   {
+//     id: 4,
+//     image: {
+//       src: 'https://bentogrids.com/cdn-cgi/image/width=1080,metadata=none,fit=scale-down,format=auto/https://s.bentogrids.com/clue0otrn0005867ta8lproze.png',
+//       width: 1080,
+//       height: 720, 
+//       format: 'webp',
+//       alt: 'Frame project'
+//     },
+//     title: 'frame',
+//     description: 'Elevate your creative workflow to an art form.',
+//   },
+]; 
